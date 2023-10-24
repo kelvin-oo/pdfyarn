3e632f37b21ad074d223b4c6a4537a88efc4b083
obisesan olamilekan <olamilekan.obisesan1@gmail.com>
2023-10-24 23:21:12 +0100

    deploying...

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -33,13 +33,13 @@ import { cn } from "@/lib/utils";
 // import SimpleBar from "simplebar-react";
 // import PdfFullscreen from "./PdfFullscreen";
 
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
   url: string;
 }
 
-const PdfRenderer = () => {
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { toast } = useToast();
 
   const [numPages, setNumPages] = useState<number>();
@@ -50,34 +50,34 @@ const PdfRenderer = () => {
 
   const isLoading = renderedScale !== scale;
 
-//   const CustomPageValidator = z.object({
-//     page: z
-//       .string()
-//       .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
-//   });
+  //   const CustomPageValidator = z.object({
+  //     page: z
+  //       .string()
+  //       .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+  //   });
 
-//   type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
+  //   type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
 
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//     setValue,
-//   } = useForm<TCustomPageValidator>({
-//     defaultValues: {
-//       page: "1",
-//     },
-//     resolver: zodResolver(CustomPageValidator),
-//   });
+  //   const {
+  //     register,
+  //     handleSubmit,
+  //     formState: { errors },
+  //     setValue,
+  //   } = useForm<TCustomPageValidator>({
+  //     defaultValues: {
+  //       page: "1",
+  //     },
+  //     resolver: zodResolver(CustomPageValidator),
+  //   });
 
-//   console.log(errors);
+  //   console.log(errors);
 
-//   const { width, ref } = useResizeDetector();
+  //   const { width, ref } = useResizeDetector();
 
-//   const handlePageSubmit = ({ page }: TCustomPageValidator) => {
-//     setCurrPage(Number(page));
-//     setValue("page", String(page));
-//   };
+  //   const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+  //     setCurrPage(Number(page));
+  //     setValue("page", String(page));
+  //   };
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
@@ -167,52 +167,11 @@ const PdfRenderer = () => {
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        {/* <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
-          <div ref={ref}>
-            <Document
-              loading={
-                <div className="flex justify-center">
-                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
-                </div>
-              }
-              onLoadError={() => {
-                toast({
-                  title: "Error loading PDF",
-                  description: "Please try again later",
-                  variant: "destructive",
-                });
-              }}
-              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-              file={url}
-              className="max-h-full"
-            >
-              {isLoading && renderedScale ? (
-                <Page
-                  width={width ? width : 1}
-                  pageNumber={currPage}
-                  scale={scale}
-                  rotate={rotation}
-                  key={"@" + renderedScale}
-                />
-              ) : null}
-
-              <Page
-                className={cn(isLoading ? "hidden" : "")}
-                width={width ? width : 1}
-                pageNumber={currPage}
-                scale={scale}
-                rotate={rotation}
-                key={"@" + scale}
-                loading={
-                  <div className="flex justify-center">
-                    <Loader2 className="my-24 h-6 w-6 animate-spin" />
-                  </div>
-                }
-                onRenderSuccess={() => setRenderedScale(scale)}
-              />
-            </Document>
-          </div>
-        </SimpleBar> */}
+        <Document
+         file={url}
+        >
+         <Page  pageNumber={1}/>
+        </Document>
       </div>
     </div>
   );
