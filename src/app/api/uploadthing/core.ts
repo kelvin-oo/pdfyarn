f183906475b65e6d5dd26f2cebfc5f91d0b83c33
obisesan olamilekan <olamilekan.obisesan1@gmail.com>
2023-10-30 19:31:08 +0100

    bug bug bug

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,26 +2,26 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { pinecone } from "@/lib/pinecone";
 const f = createUploadthing();
 
-
 export const ourFileRouter = {
-  
   pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    
     .middleware(async ({ req }) => {
-     const { getUser } = getKindeServerSession();
-     const user = getUser();
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
 
-     if (!user || !user.id) throw new Error("Unauthorized");
+      if (!user || !user.id) throw new Error("Unauthorized");
 
-    //  const subscriptionPlan = await getUserSubscriptionPlan();
+      //  const subscriptionPlan = await getUserSubscriptionPlan();
 
-     return { userId: user.id };
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      
-      const createdFIle = await db.file.create({
+      const createdFile = await db.file.create({
         data: {
           key: file.key,
           name: file.name,
@@ -30,9 +30,50 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
-      console.log("Upload complete for userId:", metadata.userId);
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        console.log("trying pinecone1");
+            const blob = await response.blob();
 
-      console.log("file url", file.url);
+            const loader = new PDFLoader(blob);
+
+            const pageLevelDocs = await loader.load();
+
+            const pagesAmt = pageLevelDocs.length;
+
+        // vectorize and index entire document
+        console.log("trying pinecone2");
+        const pineconeIndex = pinecone.Index("pdfyarn");
+        console.log("trying pinecone3");
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPEN_API_KEY,
+        });
+        console.log("trying pinecone4");
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+        });
+        console.log("trying pinecone5");
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        console.log("trying pinecone6");
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -13,25 +13,25 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
 
-// import { useResizeDetector } from "react-resize-detector";
+import { useResizeDetector } from "react-resize-detector";
 import { Button } from "./ui/button";
-// import { Input } from "./ui/input";
+import { Input } from "./ui/input";
 import { useState } from "react";
 
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-// import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
-// import SimpleBar from "simplebar-react";
-// import PdfFullscreen from "./PdfFullscreen";
+import SimpleBar from "simplebar-react";
+import PdfFullscreen from "./PdfFullscreen";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -50,34 +50,34 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
   const isLoading = renderedScale !== scale;
 
-  //   const CustomPageValidator = z.object({
-  //     page: z
-  //       .string()
-  //       .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
-  //   });
+    const CustomPageValidator = z.object({
+      page: z
+        .string()
+        .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+    });
 
-  //   type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
 
-  //   const {
-  //     register,
-  //     handleSubmit,
-  //     formState: { errors },
-  //     setValue,
-  //   } = useForm<TCustomPageValidator>({
-  //     defaultValues: {
-  //       page: "1",
-  //     },
-  //     resolver: zodResolver(CustomPageValidator),
-  //   });
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      setValue,
+    } = useForm<TCustomPageValidator>({
+      defaultValues: {
+        page: "1",
+      },
+      resolver: zodResolver(CustomPageValidator),
+    });
 
-  //   console.log(errors);
 
-  //   const { width, ref } = useResizeDetector();
 
-  //   const handlePageSubmit = ({ page }: TCustomPageValidator) => {
-  //     setCurrPage(Number(page));
-  //     setValue("page", String(page));
-  //   };
+    const { width, ref } = useResizeDetector();
+
+    const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+      setCurrPage(Number(page));
+      setValue("page", String(page));
+    };
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
@@ -85,10 +85,10 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
         <div className="flex items-center gap-1.5">
           <Button
             disabled={currPage <= 1}
-            // onClick={() => {
-            //   setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
-            //   setValue("page", String(currPage - 1));
-            // }}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+              setValue("page", String(currPage - 1));
+            }}
             variant="ghost"
             aria-label="previous page"
           >
@@ -96,7 +96,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
           </Button>
 
           <div className="flex items-center gap-1.5">
-            {/* <Input
+            <Input
               {...register("page")}
               className={cn(
                 "w-12 h-8",
@@ -107,14 +107,14 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                   handleSubmit(handlePageSubmit)();
                 }
               }}
-            /> */}
+            />
             <p className="text-zinc-700 text-sm space-x-1">
               <span>/</span>
               <span>{numPages ?? "x"}</span>
             </p>
           </div>
 
-          {/* <Button
+          <Button
             disabled={numPages === undefined || currPage === numPages}
             onClick={() => {
               setCurrPage((prev) =>
@@ -126,11 +126,11 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             aria-label="next page"
           >
             <ChevronUp className="h-4 w-4" />
-          </Button> */}
+          </Button>
         </div>
 
         <div className="space-x-2">
-          {/* <DropdownMenu>
+          <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button className="gap-1.5" aria-label="zoom" variant="ghost">
                 <Search className="h-4 w-4" />
@@ -152,7 +152,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                 250%
               </DropdownMenuItem>
             </DropdownMenuContent>
-          </DropdownMenu> */}
+          </DropdownMenu>
 
           <Button
             onClick={() => setRotation((prev) => prev + 90)}
@@ -162,18 +162,57 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             <RotateCw className="h-4 w-4" />
           </Button>
 
-          {/* <PdfFullscreen fileUrl={url} /> */}
+          <PdfFullscreen fileUrl={url} />
         </div>
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <Document
-          file={
-            "https://uploadthing-prod.s3.us-west-2.amazonaws.com/0cd53db1-b016-477a-9952-2ba098d6577a-ao6a3b.pdf"
-          }
-        >
-          <Page pageNumber={1} />
-        </Document>
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
+          <div ref={ref}>
+            <Document
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={url}
+              className="max-h-full"
+            >
+              {isLoading && renderedScale ? (
+                <Page
+                  width={width ? width : 1}
+                  pageNumber={currPage}
+                  scale={scale}
+                  rotate={rotation}
+                  key={"@" + renderedScale}
+                />
+              ) : null}
+
+              <Page
+                className={cn(isLoading ? "hidden" : "")}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={"@" + scale}
+                loading={
+                  <div className="flex justify-center">
+                    <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                  </div>
+                }
+                onRenderSuccess={() => setRenderedScale(scale)}
+              />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );
